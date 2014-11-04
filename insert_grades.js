9372714a7237b3f83b98329771aee80631b724af
Matthew Schmaus <mschmaus201@gmail.com>
2014-11-04 08:18:03 -0500

    Add grade pending image url

--- insert_grades.js
+++ insert_grades.js
@@ -1,6 +1,7 @@
 $gradeA = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_C_v2.jpg"
 $gradeB = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_B_v2.jpg"
 $gradeC = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_C_v2.jpg"
+$gradePending = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_GradePending_v2.jpg"
 
 $.each($('div.restaurant-name'), function(index, inspection) {
   $(this).append("<a href='http://www.google.com' target='_blank'><img src='' style='height:35px'/></a>");
