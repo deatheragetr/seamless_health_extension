b5c809fd3a3cc6c8762bc11e8aaa11c580cf85e3
Matthew Schmaus <mschmaus201@gmail.com>
2014-11-03 23:32:07 -0500

    Update to append link and image of letter grades

--- insert_grades.js
+++ insert_grades.js
@@ -1,5 +1,7 @@
-$('div.restaurant-name a').each(function() {
-    console.log($(this).text());
-  }
-);
+$gradeA = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_C_v2.jpg"
+$gradeB = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_B_v2.jpg"
+$gradeC = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_C_v2.jpg"
 
+$.each($('div.restaurant-name'), function(index, inspection) {
+  $(this).append("<a href='http://www.google.com' target='_blank'><img src='' style='height:35px'/></a>");
+});
