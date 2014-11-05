c54632a4a38c568cbde717b34a5003397e9a11d4
Matthew Schmaus <mschmaus201@gmail.com>
2014-11-04 21:20:44 -0500

    Add functionality for parsing addresses per restaurant listed

--- insert_grades.js
+++ insert_grades.js
@@ -1,8 +1,13 @@
-$gradeA = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_C_v2.jpg"
-$gradeB = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_B_v2.jpg"
-$gradeC = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_C_v2.jpg"
-$gradePending = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_GradePending_v2.jpg"
+gradeA = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_C_v2.jpg"
+gradeB = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_B_v2.jpg"
+gradeC = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_C_v2.jpg"
+gradePending = "http://parmenides.wnyc.org/media/photologue/photos/Grade%20Card_GradePending_v2.jpg"
 
 $.each($('div.restaurant-name'), function(index, inspection) {
-  $(this).append("<a href='http://www.google.com' target='_blank'><img src='' style='height:35px'/></a>");
+  var linkTitle = $(this).find('a').attr("title");
+  var address = linkTitle.split("</span>", 2)[1].split("<br />", 1);
+
+  // Send address to server, wait for letter grade response
+  var letterGrade = gradeB
+  $(this).append("<a href='http://www.google.com' target='_blank'><img src='" + letterGrade + "' style='height:35px'/></a>");
 });
