2804787acc4ea60604cb0a8a4fc04658c35ecf76
Matthew Schmaus <mschmaus201@gmail.com>
2014-11-09 16:47:40 -0500

    Add ajax request for each restaurant name

--- insert_grades.js
+++ insert_grades.js
@@ -1,11 +1,24 @@
 $.each($('div.restaurant-name'), function(index, inspection) {
   var $restaurantLink = $(this).find('a');
-
-  var restaurantName = $restaurantLink.text().trim();
   var linkHref = $restaurantLink.attr('href');
   var vendorId = linkHref.split('.')[1];
 
-  // Send address to server, wait for letter grade response
-  var letterGrade // based on response from server
-  $(this).append("<a href='http://www.google.com' target='_blank'><img src='" + letterGrade + "' style='height:35px'/></a>");
+  $.ajax({
+    url: 'server.com',
+    data: {
+      'vendorId': vendorId,
+      'restaurantHref': linkHref
+    },
+    type: 'POST',
+    success: function(response) {
+      var letterGrade = response['letterGrade']; // based on response from server
+      $(this).append(
+        "<a href='http://www.google.com' target='_blank'>" +
+        "<img src='" + letterGrade + "' style='height:35px'/></a>"
+        );
+    },
+    error: function(response) {
+      // Should we do anything?
+    }
+  })
 });
