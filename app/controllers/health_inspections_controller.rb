d534dc124f1784dcd417f963c98727907888b064
Matt Schmaus <mschmaus201@gmail.com>
2014-11-18 23:44:47 -0500

    Fix cache writing

--- app/controllers/health_inspections_controller.rb
+++ app/controllers/health_inspections_controller.rb
@@ -20,10 +20,7 @@ class HealthInspectionsController < ApplicationController
     end
 
     resp['grades_not_found'] = params["json"].keys - resp['grades_found'].keys
-
-    resp['grades_not_found'].each do |id|
-      Rails.cache.write(id) { id }
-    end
+    resp['grades_not_found'].each { |id| Rails.cache.write(id) }
 
     render :json => resp
   end
