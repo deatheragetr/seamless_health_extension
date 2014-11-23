fac88a48ed927a17a2f1e522d2de296e7ce113b9
Thomas Deatherage <deatheragetr@vcu.edu>
2014-11-22 20:34:49 -0500

    make top level root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post '/health_inspections' => 'health_inspections#show_to_extension'
   get '/health_inspections/:vendor_id' => 'health_inspections#show_to_webview'
   get '/health_inspections' => 'health_inspections#index'
+  root 'health_inspections#index'
 end
