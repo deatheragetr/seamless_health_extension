0b214e2d99dddbf5d98a8fdaf51042e2b81f37af
Matt Schmaus <mschmaus201@gmail.com>
2014-12-08 23:45:33 -0500

    Refactoring of server logic

--- app/controllers/health_inspections_controller.rb
+++ app/controllers/health_inspections_controller.rb
@@ -1,11 +1,11 @@
 class HealthInspectionsController < ApplicationController
 
-  layout "show_page"
+  layout 'show_page'
   skip_before_filter :verify_authenticity_token
 
   def index
-    @best = HealthInspection.where('seamless_vendor_id IS NOT NULL').where("grade = 'A'").order('inspection_date desc').limit(30).to_a.uniq { |inspection| inspection.dba }
-    @worst = HealthInspection.where('seamless_vendor_id IS NOT NULL').where("grade = 'C'").order('inspection_date desc').limit(30).to_a.uniq { |inspection| inspection.dba }
+    @best = HealthInspection.index_page_inspections('A')
+    @worst = HealthInspection.index_page_inspections('C')
   end
 
   def show_to_recently_ordered
@@ -15,13 +15,7 @@ class HealthInspectionsController < ApplicationController
       vendor_ids_and_links[vendor_id] = hash['restaurantLink']
     end
 
-    inspections = HealthInspection.query_or_fetch_all(vendor_ids_and_links).to_a
-    inspections = inspections.select! {|insp| !insp.grade.nil?  } || []
-
-    inspections.sort! { |insp1, insp2| insp2.inspection_date <=> insp1.inspection_date } \
-               .uniq! { |insp| insp.seamless_vendor_id }
-
-    inspections ||= []
+    inspections = HealthInspection.query_or_fetch_all(vendor_ids_and_links) || []
 
     response_json = {'grades_found' => {}}.tap do |rsp|
       inspections.each do |insp|
@@ -46,15 +40,9 @@ class HealthInspectionsController < ApplicationController
   end
 
   def show_to_extension
-    inspections = HealthInspection.query_or_fetch_all(params["json"]).to_a
-    inspections = inspections.select! {|insp| !insp.grade.nil?  } || []
+    inspections = HealthInspection.query_or_fetch_all(params["json"]) || []
 
-    inspections.sort! { |insp1, insp2| insp2.inspection_date <=> insp1.inspection_date } \
-      .uniq! { |insp| insp.seamless_vendor_id }
-
-    inspections ||= []
-
-    resp = {'grades_found' => {}}.tap do |rsp|
+    response_json = {'grades_found' => {}}.tap do |rsp|
       inspections.each do |insp|
         rsp['grades_found'][insp.seamless_vendor_id.to_s] =  {
           url: "http://seamless-health-grades.herokuapp.com/health_inspections/#{insp.seamless_vendor_id}",
@@ -63,15 +51,17 @@ class HealthInspectionsController < ApplicationController
       end
     end
 
-    resp['grades_not_found'] = params["json"].keys - resp['grades_found'].keys
-    resp['grades_not_found'].each { |id| Rails.cache.write(id, id) }
+    response_json['grades_not_found'] = params["json"].keys - response_json['grades_found'].keys
+    response_json['grades_not_found'].each { |id| Rails.cache.write(id, id) }
 
-    render :json => resp
+    render :json => response_json
   end
 
   def show_to_webview
-    inspections = HealthInspection.where("seamless_vendor_id = #{params[:vendor_id]} AND violation_description is NOT NULL") \
+    inspections = HealthInspection.where("seamless_vendor_id = #{params[:vendor_id]}") \
+      .where('violation_description is NOT NULL') \
       .sort { |insp1, insp2| insp2.inspection_date <=> insp1.inspection_date }
+
     most_recent_inspection = inspections.first
 
     @restaurant_name = most_recent_inspection.dba
--- app/models/health_inspection.rb
+++ app/models/health_inspection.rb
@@ -3,9 +3,9 @@ class HealthInspection < ActiveRecord::Base
   def self.query_or_fetch_all(params)
     vendor_ids = params.keys
     inspections = where("seamless_vendor_id IN (#{vendor_ids.join(', ')})")
+
     populated_vendor_ids = inspections.collect(&:seamless_vendor_id).uniq.map(&:to_s)
     missing_vendor_ids = vendor_ids - populated_vendor_ids
-
     cached_do_not_call_ids = missing_vendor_ids.map { |id| Rails.cache.fetch(id) }.compact
     noncached_missing_vendor_ids = missing_vendor_ids - cached_do_not_call_ids
 
@@ -20,8 +20,23 @@ class HealthInspection < ActiveRecord::Base
         ActiveRecord::Base.connection.close
       end
     end
+
     threads.each(&:join)
-    inspections
+    filter_and_sort(inspections.to_a)
+  end
+
+  def self.index_page_inspections(letter_grade, cuisine_description=nil)
+    inspections = self.where('seamless_vendor_id IS NOT NULL') \
+      .where("grade = '#{letter_grade}'")
+
+    if cuisine_description
+      inspections = inspections.where("cuisine_description = '#{cuisine_description}'")
+    end
+
+    inspections.order('inspection_date desc') \
+      .limit(30) \
+      .to_a \
+      .uniq { |inspection| inspection.dba }
   end
 
   def address
@@ -31,4 +46,16 @@ class HealthInspection < ActiveRecord::Base
   def encoded_address
     address.gsub(' ', '+')
   end
+
+  private
+    def self.filter_and_sort(inspections)
+      inspections = inspections.select! {|insp| !insp.grade.nil?  } || []
+      inspections.sort! { |insp1, insp2| insp2.inspection_date <=> insp1.inspection_date } \
+        .uniq! { |insp| insp.seamless_vendor_id }
+    end
+
+    def self.restaurant_phone_number(url)
+      SeamlessClient.new(url).phone
+    end
+
 end
